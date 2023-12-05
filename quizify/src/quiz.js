12c89f729f0fa7d88589c8f58544049c53200615
eyahn <eyahn@umich.edu>
2023-12-05 15:45:57 -0500

    evelyn 12/5 3:45

--- quizify/src/quiz.js
+++ quizify/src/quiz.js
@@ -21,28 +21,26 @@ const cityQuiz = {
     genre: ""
 }
 
+// Taste: Question 1
+
 const tasteOne = document.querySelector('#taste-answer-1');
 tasteOne.addEventListener('click', () => {
     clickAnswer(tasteOne);
-    tasteQuiz.energy = '0.2';
 });
 
 const tasteTwo = document.querySelector('#taste-answer-2');
 tasteTwo.addEventListener('click', () => {
     clickAnswer(tasteTwo);
-    tasteQuiz.energy = '0.4';
 });
 
 const tasteThree = document.querySelector('#taste-answer-3');
 tasteThree.addEventListener('click', () => {
     clickAnswer(tasteThree);
-    tasteQuiz.energy = '0.7';
 })
 
 const tasteFour = document.querySelector('#taste-answer-4');
 tasteFour.addEventListener('click', () => {
     clickAnswer(tasteFour);
-    tasteQuiz.energy = '0.7';
 })
 
 const tasteFive = document.querySelector('#taste-answer-5');
@@ -57,6 +55,48 @@ tasteSix.addEventListener('click', () => {
     tasteQuiz.energy = '0.7';
 })
 
+// Taste: Question 2
+
+const tasteSeven = document.querySelector('#taste-answer-7');
+tasteSeven.addEventListener('click', () => {
+    tasteQuiz.energy = '0.2';
+    clickAnswer(tasteSeven);
+})
+
+const tasteEight = document.querySelector('#taste-answer-8');
+tasteEight.addEventListener('click', () => {
+    tasteQuiz.energy = '0.85';
+    clickAnswer(tasteEight);
+})
+
+const tasteNine = document.querySelector('#taste-answer-9');
+tasteNine.addEventListener('click', () => {
+    tasteQuiz.energy = '1';
+    clickAnswer(tasteNine);
+})
+
+const tasteTen = document.querySelector('#taste-answer-10');
+tasteTen.addEventListener('click', () => {
+    tasteQuiz.energy = '0.6';
+    clickAnswer(tasteTen);
+})
+
+const tasteEleven = document.querySelector('#taste-answer-11');
+tasteEleven.addEventListener('click', () => {
+    tasteQuiz.energy = '0.35';
+    clickAnswer(tasteEleven);
+})
+
+const tasteTwelve = document.querySelector('#taste-answer-12');
+tasteTwelve.addEventListener('click', () => {
+    tasteQuiz.energy = '0';
+    clickAnswer(tasteTwelve);
+})
+
+// Taste: Question 3
+
+
+
 function clickAnswer(selectedItem) {
     const tasteQuestion = selectedItem.parentElement.querySelectorAll('div');
     tasteQuestion.forEach((div) => {
@@ -64,7 +104,7 @@ function clickAnswer(selectedItem) {
             div.classList.remove('currentAnswer')
         }
     })
-    console.log(selectedItem);
+    console.log(tasteQuiz);
     selectedItem.classList.add('currentAnswer');
     tasteQuestion.forEach((div) => {
         div.classList.remove('notSelected');
