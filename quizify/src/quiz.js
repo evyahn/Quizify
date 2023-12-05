abc13929a96a9a54d0580bc8e1be29efe09a6d2f
Grace W <gwhaley@umich.edu>
2023-12-05 14:49:58 -0500

    quiz js dicts

---
+++ quizify/src/quiz.js
@@ -0,0 +1,23 @@
+
+const tasteQuiz = {
+    energy: "",
+    genre: "",
+    popularity: "",
+    speechiness: ""
+}
+
+const moodQuiz = {
+    energy: "",
+    genre: "",
+    popularity: ""
+}
+
+const cityQuiz = {
+    energy: "",
+    valence: "",
+    dance: "",
+    tempo: "",
+    popularity: "",
+    speechiness: "",
+    genre: ""
+}
