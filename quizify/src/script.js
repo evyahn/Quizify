9623a5d456b9c24e13755c9c5406e6a24c5bdce0
eyahn <eyahn@umich.edu>
2024-02-22 11:57:28 -0500

    redirect uri change

--- quizify/src/script.js
+++ quizify/src/script.js
@@ -24,7 +24,8 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/quizchoice");
+    // params.append("redirect_uri", "http://localhost:5173/quizchoice");
+    params.append("redirect_uri", "https://evyahn.github.io/Quizify/quizify/quizchoice");
     params.append("scope", "user-read-private user-read-email playlist-modify-public");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -39,7 +40,8 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/quizchoice");
+    // params.append("redirect_uri", "http://localhost:5173/quizchoice");
+    params.append("redirect_uri", "https://evyahn.github.io/Quizify/quizify/quizchoice");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
